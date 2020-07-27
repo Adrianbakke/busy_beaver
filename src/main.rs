406c318958e81bf27cedf234bb9c5ce1a5fce4ca
Adrianbakke <adrian.bakke@yahoo.no>
2020-07-27 23:25:12 +0200

    add possibility to change num of symbols + refactor

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,7 @@ enum Direction {
 
 #[derive(Debug, Clone, Copy)]
 /// The choosen symbols for our tw that can be written on the tape
-enum Symbol {
-    Zero,
-    One,
-}
+struct Symbol(u8);
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 /// States of the the Turing Machine
@@ -37,7 +34,7 @@ struct Cards(Vec<Card>);
 
 #[derive(Debug, Clone)]
 /// Defines a card holding instructions.
-struct Card([Instructions; 2]);
+struct Card(Vec<Instructions>);
 
 #[derive(Debug, Clone, Copy)]
 /// Holds the instruction the tw must follow
@@ -57,10 +54,11 @@ struct Typewriter {
     head: usize,
 }
 
+#[derive(Debug)]
 /// Holds all the generated machines
 struct Machines(Vec<Cards>);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 /// Keep track of stats during a run of busy beaver
 struct Stats {
     cards:          Cards,
@@ -77,7 +75,7 @@ impl Instructions {
 }
 
 impl Card {
-    pub fn new(instructions: [Instructions; 2]) -> Self {
+    pub fn new(instructions: Vec<Instructions>) -> Self {
         Self(instructions)
     }
 
@@ -86,8 +84,7 @@ impl Card {
     pub fn get_instruction<'a>(
         &'a self, tw: &Typewriter, board: &Board) -> &'a Instructions {
         match tw.read(board) {
-            Symbol::Zero => &self.0[0],
-            Symbol::One  => &self.0[1],
+            Symbol(i) => &self.0[i as usize],
         }
     }
 }
@@ -109,6 +106,10 @@ impl Cards {
         }
         states
     }
+
+    pub fn contains_halt_state(&self) -> bool {
+        self.get_states().contains(&State::Halt)
+    }
 }
 
 impl Typewriter {
@@ -128,80 +129,69 @@ impl Typewriter {
     /// Writes the new symbol onto the board.
     pub fn write(&self, sym: &Symbol, board: &mut Board) {
         match sym {
-            Symbol::Zero => board.0[self.head] = 0,
-            Symbol::One  => board.0[self.head] = 1,
+            Symbol(i) => board.0[self.head] = *i,
         }
     }
 
     /// Reads symbol of the board of which the tw head is hovering.
     pub fn read(&self, board: &Board) -> Symbol {
-        match board.0[self.head] {
-            0 => Symbol::Zero,
-            1 => Symbol::One,
-            _ => panic!("Undefined symbol for this input")
-        }
+        Symbol(board.0[self.head])
     }
 }
 
 impl Machines {
-    pub fn generate(num_cards: usize) -> Self {
+    pub fn generate(num_cards: usize, num_symbols: usize) -> Self {
         // construct every possible set of instructions
-        let inst1 = Self::generate_instructions(num_cards);
-        let inst2 = inst1.clone();
+        let inst = Self::generate_instructions(num_cards, num_symbols);
 
         // constructs all possible cards
-        let mut cards = vec![];
-        for i1 in inst1.iter() {
-            for i2 in inst2.iter() {
-                let card = Card::new([*i1,*i2]);
-                cards.push(card);
-            }
-        }
+        let cards = Self::generate_all_possible_cards(inst, num_symbols);
 
         // constructs all possible machines that contains the halt state
-        let machines = Self::generate_all_possible_machines(cards, num_cards);
+        let num_configs = 
+            (num_symbols*2*(num_cards+1)).pow((num_cards+num_symbols) as u32);
+
+        println!("Starts creating machines");
+        let machines =
+            Self::generate_all_possible_machines(cards, num_cards,
+                                                 num_configs);
 
         Self(machines)
     }
 
-    fn combine_cards(cards: &Vec<Card>,
-                     num_cards: usize,
-                     mut ind: usize,
-                     r: usize,
-                     mut cards_holder: &mut Vec<Card>,
-                     mut res: &mut Vec<Cards>) {
-        while ind < cards.len() {
-            cards_holder.push(cards[ind].clone());
-                
-            if r < num_cards {
-                Self::combine_cards(
-                    cards, num_cards, 0, r+1, &mut cards_holder, &mut res);
-            } else {
-                let card = Cards::new(cards_holder.to_vec());
-                if Self::contains_halt_state(&card) {
-                    res.push(card);
-                }
-            }
-            cards_holder.pop();
-            ind += 1;
-        } 
-    }
-
-    fn generate_all_possible_machines(cards: Vec<Card>,
-                                      num_cards: usize) -> Vec<Cards> {
-        let mut res: Vec<Cards>        = Vec::new();
-        let mut card_holder: Vec<Card> = Vec::new();
-        Self::combine_cards(&cards, num_cards-1, 0, 0,
-                            &mut card_holder, &mut res);
+    fn generate_all_possible_cards(inst: Vec<Instructions>,
+                                   num_symbols: usize) -> Vec<Card> {
+        let mut res: Vec<Vec<Instructions>>    = Vec::new();
+        let mut inst_holder: Vec<Instructions> = Vec::new();
+        generate_combinations(&inst, num_symbols, 0, 0,
+                              &mut inst_holder, &mut res);
+        let res: Vec<Card> = res.iter()
+                                .map(|x| Card::new(x.clone()))
+                                .collect();
         res
     }
 
-    fn contains_halt_state(cards: &Cards) -> bool {
-        cards.get_states().contains(&State::Halt)
+    fn generate_all_possible_machines(cards: Vec<Card>,
+                                      num_cards: usize,
+                                      num_configs: usize) -> Vec<Cards> {
+        let mut res: Vec<Vec<Card>>    = Vec::with_capacity(num_configs);
+        let mut card_holder: Vec<Card> = Vec::new();
+        println!("memory allocated, proceeding...");
+        generate_combinations(&cards, num_cards, 0, 0,
+                              &mut card_holder, &mut res);
+        let res: Vec<Cards> =
+            res.into_iter().map(|x| Cards::new(x)).collect();
+        res.into_iter() .filter(|x| x.contains_halt_state()).collect()
     }
 
-    fn generate_instructions(num_cards: usize) -> Vec<Instructions> {
-        let sym = [Symbol::Zero, Symbol::One];
+
+    fn generate_instructions(num_cards: usize,
+                             num_symbols: usize) -> Vec<Instructions> {
+        let mut sym = vec![];
+        for i in 0..num_symbols {
+            sym.push(Symbol(i as u8));
+        }
+
         let dir = [Direction::Left, Direction::Right];
         let mut states = vec![State::Halt];
         for i in 0..num_cards {
@@ -223,6 +213,13 @@ impl Machines {
 impl Stats {
     pub fn init(cards: Cards) -> Self {
         Self { cards, action: 0, score: 0,
+               boards: vec![], head_positions: vec![] }
+    }
+    
+    pub fn default() -> Self {
+        let inst = Instructions::new(Symbol(0), Direction::Left, State::Halt);
+        let card = Card::new(vec![inst]);
+        Self { cards: Cards::new(vec![card]), action: 0, score: 0,
                boards: vec![], head_positions: vec![] }
     }
 
@@ -257,6 +254,25 @@ impl Stats {
         }
         println!("");
     }
+}
+
+fn generate_combinations<T: Clone>(input: &Vec<T>,
+                              max_depth: usize,
+                              mut ind: usize,
+                              r: usize,
+                              mut input_holder: &mut Vec<T>,
+                              mut res: &mut Vec<Vec<T>>) {
+    while ind < input.len() {
+        input_holder.push(input[ind].clone());
+        if r < max_depth-1 {
+            generate_combinations(
+                input, max_depth, 0, r+1, &mut input_holder, &mut res);
+        } else {
+            res.push(input_holder.clone());
+        }
+        input_holder.pop();
+        ind += 1;
+     }
 }
 
 fn busy_beaver(cards: &Cards) -> Option<Stats> {
@@ -302,22 +318,42 @@ fn highest_action(stats: &Vec<Stats>) -> &Stats {
     current_leader
 }
 
-//TODO: make symbols dynamic + comment all code
+//TODO: comment all code
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("{:?}", args);
 
-    let m = Machines::generate(args[args.len()-1].parse().unwrap());
-    
-    let mut stats_holder = vec![];
+    let arg1 = args[args.len()-2].parse().unwrap();
+    let arg2 = args[args.len()-1].parse().unwrap();
 
-    for c in m.0.iter() {
+    let m = Machines::generate(arg1, arg2);
+
+    println!("Machines generated, moving on to the BUSY BEAVER!");
+    
+    let mut best_score = 0;
+    let mut best_score_holder = Stats::default();
+    let mut best_action = 0;
+    let mut best_action_holder = Stats::default();
+
+    for (x,c) in m.0.iter().enumerate() {
         if let Some(stats) = busy_beaver(&c) {
-            stats_holder.push(stats);
+            if stats.action > best_action {
+                best_action_holder = stats.clone();
+                best_action = stats.action;
+            }
+            if stats.score > best_score {
+                best_score_holder = stats.clone();
+                best_score = stats.score;
+            }
+            //stats_holder.push(stats);
         }
     }
-
+    
+    println!("winner_score  - score: {:?}  -  action: {:?}",
+        best_score_holder.score, best_score_holder.action);
+    best_score_holder.show_state();
+    /*
     let winner_score = highest_score(&stats_holder);
     let winner_action = highest_action(&stats_holder);
  
@@ -328,5 +364,7 @@ fn main() {
     println!("winner_score  -  cards: {:?}  -  score: {:?}  -  action: {:?}",
         winner_action.cards, winner_action.score, winner_action.action);
     winner_action.show_state();
+    */
+
 }
 
